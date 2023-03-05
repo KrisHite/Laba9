dbec168b046f0716ef53ff1bb0d95b585faf40e0
KrisHite <alexgusevmail@gmail.com>
2023-03-05 19:17:33 +0500

    Первые пять примеров

--- src/Examples/Example_2.java
+++ src/Examples/Example_2.java
@@ -1,4 +1,15 @@
 package Examples;
 
 public class Example_2 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            System.out.println("1");
+            throw new RuntimeException("Непроверяемая ошибка");
+        } catch (Exception e) {
+            System.out.println("2 " + e);
+        }
+        System.out.println("3");
+    }
+
 }
--- src/Examples/Example_3.java
+++ src/Examples/Example_3.java
@@ -1,4 +1,17 @@
 package Examples;
 
 public class Example_3 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("ошибка");
+        } catch (NullPointerException e) {
+            System.out.println("1");
+        } catch (RuntimeException e) {//Этот перехватывает исключение
+            System.out.println("2");
+        } catch (Exception e) {
+            System.out.println("3");
+        }
+        System.out.println("4");
+    }
 }
